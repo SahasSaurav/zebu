2eaca25c371251efafd5c5277b4450399a9bb2d1
Sahas <sahas@miniventurelabs.com>
2021-12-07 14:04:24 +0530

    uild dir

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route, Navigate} from 'react-router-dom'
+import CreateProject from './component/CreateProject'
+import LabelImages from './component/LabelImages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/create-project" />} />
+      <Route path="/create-project" element={<CreateProject />} />
+      <Route path="/label-images" element={< LabelImages />} />
+    </Routes>
   );
 }
 
